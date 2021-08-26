75484c9a4ebe5c38cc3c3bf02b49b293a76e4534
Diego <diegsagdemig@hotmail.com>
2021-08-26 02:37:58 +0200

    Demo Level blockout finished and playable

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     [HideInInspector] public bool gameOver;
+
+    private bool timeScaledecreasing;
 
     public static GameManager Instance;
 
     private int currentPoints;
 
     [SerializeField] private List<CoinBehaviour> coins;
+
+    private float noCoinsTimeCounter;
+
+    [SerializeField] private float noCoinsMaxTime = 40;
+
+    private float resetTimeCounter;
+
+    [SerializeField] private float resetMaxTime = 1;
+
+    private bool alreadyReloaded;
 
     private void Awake()
     {
@@ -27,17 +41,44 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         if (gameOver)
-            Time.timeScale = 0;
+            timeScaledecreasing = true;
+
+        if (timeScaledecreasing)
+            Time.timeScale -= Time.deltaTime;
 
         if (currentPoints >= coins.Count)
         {
             gameOver = true;
             Debug.Log("VICTORY!");
         }
+
+        noCoinsTimeCounter += Time.deltaTime;
+
+        if(noCoinsTimeCounter > noCoinsMaxTime)
+            Debug.Log("Hold [R] to restart");
+
+        if (Input.GetKey(KeyCode.R))
+            resetTimeCounter += Time.deltaTime;
+        else
+            resetTimeCounter = 0;
+
+        if (resetTimeCounter > resetMaxTime)
+            ReloadScene();
+    }
+
+    private void ReloadScene()
+    {
+        if (alreadyReloaded)
+            return;
+
+        alreadyReloaded = true;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void AddPoint()
     {
+        Debug.Log(noCoinsTimeCounter);
+        noCoinsTimeCounter = 0;
         currentPoints++;
     }
 
