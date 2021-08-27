2ce61f66442d78a77fd83f57d9b0512a8324b37a
Diego <diegsagdemig@hotmail.com>
2021-08-27 19:46:06 +0200

    Added controls screen

--- Assets/Scripts/MenuCarsControl.cs
+++ Assets/Scripts/MenuCarsControl.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuCarsControl : MonoBehaviour
 {
@@ -9,9 +12,15 @@ public class MenuCarsControl : MonoBehaviour
 
     [SerializeField] private float zoomOutSpeed;
 
+    [SerializeField] private Image controlsImg;
+
+    [SerializeField] private TextMeshProUGUI title;
+
     private float startTime;
 
     private Camera cam;
+
+    private bool showingControls;
 
     // Start is called before the first frame update
     void Start()
@@ -31,10 +40,29 @@ public class MenuCarsControl : MonoBehaviour
         if (Time.time - startTime < 3)
             return;
 
-        GameManager.Instance.ShowText("[ESPACIO] PARA JUGAR", true);
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (!showingControls)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            GameManager.Instance.ShowText("[ESPACIO] PARA CONTINUAR", true);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+                ShowControls();
         }
+        else
+        {
+            GameManager.Instance.ShowText("[ESPACIO] PARA JUGAR", true);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    private void ShowControls()
+    {
+        GameManager.Instance.ShowText("");
+        startTime = Time.time;
+        showingControls = true;
+
+        controlsImg.enabled = true;
+        title.enabled = false;
     }
 }
